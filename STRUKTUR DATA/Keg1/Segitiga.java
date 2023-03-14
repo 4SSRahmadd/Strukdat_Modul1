19a7f0a3f0ce5853b4b327b7f6d0221542b097b8
Yaasir Rahmat Kautsar <96896573+4SSRahmadd@users.noreply.github.com>
2023-03-14 22:21:25 +0700

    keg 1 ygy
    ho oh tenann

---
+++ STRUKTUR DATA/Keg1/Segitiga.java
@@ -0,0 +1,27 @@
+package Keg1;
+
+public class Segitiga<T> {
+    private T alas;
+    private T tinggi;
+
+    public Segitiga(T alas, T tinggi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+    public T getAlas() {
+        return alas;
+    }
+
+    public T getTinggi() {
+        return tinggi;
+    }
+
+    public int getResultAsInt(){
+        return ((Integer)alas*(Integer)tinggi/2);
+    }
+
+    public double getResultAsDouble(){
+        return ((Double)alas*(Double)tinggi/2);
+    }
+}
